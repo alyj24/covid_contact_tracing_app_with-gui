43f3fefdf97cdffeec4096422ed4c4a0306938c9
Family <albon.136546120855@2depedqc.ph>
2023-07-29 01:22:25 +0800

    pseudocode for the program with GUI

---
+++ ccta_gui.py
@@ -0,0 +1,12 @@
+# pseudocode
+# import the tkinter
+# import the existed program for the new class
+# implement the functions of the class with GUI
+# establish the constructor
+# construct the labels and methods
+# provide instance
+# add entry
+# search entry
+# provide instance
+# check errors
+# run the program
