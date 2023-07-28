79b12071390e9b4f66ce7144cb72325f0f6d05ac
Family <albon.136546120855@2depedqc.ph>
2023-07-29 01:12:25 +0800

    pseudocode

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ print("Final Project".center(38, "="))
 print("\033[93m=" * 38)
 print("\033[95mCOVID-19 CONTACT TRACING APP WITH GUI")
 print("\033[93m=" * 38)
+
+# pseudocode
+# import a text file
+# create the class that will represent the app
+# establish the functions
+
