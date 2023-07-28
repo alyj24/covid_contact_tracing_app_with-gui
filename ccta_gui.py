cef1990fb7e406dcc2e814708ef0143ba7d46d2d
Family <albon.136546120855@2depedqc.ph>
2023-07-29 01:25:08 +0800

    from, import

--- ccta_gui.py
+++ ccta_gui.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 
 # import the existed program for the new class
+from main import Covid19ContactTracingApp
 
 # implement the functions of the class with GUI
 # establish the constructor
