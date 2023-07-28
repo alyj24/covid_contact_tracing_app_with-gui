56f25e9e9288b8e5226e72e6cef07a72cd45e112
Family <albon.136546120855@2depedqc.ph>
2023-07-29 02:31:13 +0800

    finalization

--- ccta_gui.py
+++ ccta_gui.py
@@ -1,79 +1,78 @@
 # pseudocode
 # import the tkinter
 import tkinter as tk
+from main import CCTracingApp
 
-# import the existed program for the new class
-from main import Covid19ContactTracingApp
-
-# implement the functions of the class with GUI
-class C19CTAppGUI:
+# implement the functions of class with GUI
+class CovidContactTracingAppGUI:
     # establish constructor
     def __init__(self, root):
         self.root = root
         self.root.title("COVID-19 Contact Tracing App")
         self.root.geometry("400x200")
 
-    # construct the labels and methods
-    self.name_label = tk.Label(root, text="Name: ", font=("Times New Roman", 11, "bold"), bg="#8470FF")
-    self.name_entry = tk.Entry(root)
+        # construct labels and methods
+        self.name_label = tk.Label(root, text="Name: ", font=("Times New Roman", 11, "bold"), bg="#8470FF")
+        self.name_entry = tk.Entry(root)
 
-    self.address_label = tk.Label(root, text="Address: ", font=("Times New Roman", 11, "bold"), bg="#FFB6C1")
-    self.address_entry = tk.Entry(root)
+        self.address_label = tk.Label(root, text="Address: ", font=("Times New Roman", 11, "bold"), bg="#FFB6C1")
+        self.address_entry = tk.Entry(root)
 
-    self.birthday_label = tk.Label(root, text="Date of Birth: ", font=("Times New Roman", 11, "bold"), bg="#B0E2FF")
-    self.birthday_label_entry = tk.Entry(root)
+        self.birthday_label = tk.Label(root, text="Date of Birth: ", font=("Times New Roman", 11, "bold"), bg="#B0E2FF")
+        self.birthday_label_entry = tk.Entry(root)
 
-    self.gender_label = tk.Label(root, text="Gender: ", font=("Times New Roman", 11, "bold"), bg="#EEA2AD")
-    self.gender_entry = tk.Entry(root)
+        self.gender_label = tk.Label(root, text="Gender: ", font=("Times New Roman", 11, "bold"), bg="#EEA2AD")
+        self.gender_entry = tk.Entry(root)
 
-    self.number_label = tk.Label(root, text="Phone Number: ", font=("Times New Roman", 11, "bold"), bg="#F08080")
-    self.number_entry = tk.Entry(root)
+        self.phone_label = tk.Label(root, text="Phone Number: ", font=("Times New Roman", 11, "bold"), bg="#F08080")
+        self.phone_entry = tk.Entry(root)
 
-    self.email_label = tk.Label(root, text="Email Address: ", font=("Times New Roman", 11, "bold"), bg="#E4CCB4")
-    self.email_entry = tk.Entry(root)
+        self.email_label = tk.Label(root, text="Email Address: ", font=("Times New Roman", 11, "bold"), bg="#E4CCB4")
+        self.email_entry = tk.Entry(root)
 
-    self.exposed_label = tk.Label(root, text="Haye you ever been exposed recently with COVID-19 Patients? ", font=("Times New Roman", 11, "bold"), bg="#FFF68F")
-    self.exposed_entry = tk.Entry(root)
+        self.exposed_label = tk.Label(root, text="Haye you ever been exposed recently with COVID-19 Patients? ", font=("Times New Roman", 11, "bold"), bg="#FFF68F")
+        self.exposed_entry = tk.Entry(root)
 
-    self.symptoms_label = tk.Label(root, text="Do you experience currently symptoms of COVID-19? ", font=("Times New Roman", 11, "bold"), bg="#98F5FF")
-    self.symptoms_entry = tk.Entry(root)
+        self.symptoms_label = tk.Label(root, text="Do you experience currently symptoms of COVID-19? ", font=("Times New Roman", 11, "bold"), bg="#98F5FF")
+        self.symptoms_entry = tk.Entry(root)
 
-    # signify buttons
-    self.add_button = tk.Button(root, text="Add Entry", command=self.add_entry)
-    self.search_button = tk.Button(root, text="Search Entry", command=self.search_entry)
+        # Create buttons
+        self.add_button = tk.Button(root, text="Add Entry", command=self.add_entry)
+        self.search_button = tk.Button(root, text="Search Entry", command=self.search_entry)
 
-    # initiate labels
-    self.name_label.grid(row=0, column=0, padx=10, pady=5, sticky=tk.W)
-    self.name_entry.grid(row=0, column=1, pady=10, padx=5)
+        # Place labels and entry fields on grid
+        self.name_label.grid(row=0, column=0, padx=10, pady=5, sticky=tk.W)
+        self.name_entry.grid(row=0, column=1, pady=10, padx=5)
 
-    self.address_label.grid(row=5, column=0, padx=10, pady=5, sticky=tk.W)
-    self.address_entry.grid(row=5, column=1, pady=10, padx=5)
+        self.address_label.grid(row=5, column=0, padx=10, pady=5, sticky=tk.W)
+        self.address_entry.grid(row=5, column=1, pady=10, padx=5)
 
-    self.birthday_label.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
-    self.birthday_label_entry.grid(row=1, column=1, pady=10, padx=5)
+        self.birthday_label.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
+        self.birthday_label_entry.grid(row=1, column=1, pady=10, padx=5)
 
-    self.gender_label.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
-    self.gender_entry.grid(row=2, column=1, pady=10, padx=5)
+        self.gender_label.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
+        self.gender_entry.grid(row=2, column=1, pady=10, padx=5)
 
-    self.phone_label.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
-    self.phone_entry.grid(row=3, column=1, pady=10, padx=5)
+        self.phone_label.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
+        self.phone_entry.grid(row=3, column=1, pady=10, padx=5)
 
-    self.email_label.grid(row=4, column=0, padx=10, pady=5, sticky=tk.W)
-    self.email_entry.grid(row=4, column=1, pady=10, padx=5)
+        self.email_label.grid(row=4, column=0, padx=10, pady=5, sticky=tk.W)
+        self.email_entry.grid(row=4, column=1, pady=10, padx=5)
 
-    self.exposed_label.grid(row=6, column=0, padx=10, pady=5, sticky=tk.W)
-    self.exposed_entry.grid(row=6, column=1, pady=10, padx=5)
+        self.exposed_label.grid(row=6, column=0, padx=10, pady=5, sticky=tk.W)
+        self.exposed_entry.grid(row=6, column=1, pady=10, padx=5)
 
-    self.symptoms_label.grid(row=7, column=0, padx=10, pady=5, sticky=tk.W)
-    self.symptoms_entry.grid(row=7, column=1, pady=10, padx=5)
+        self.symptoms_label.grid(row=7, column=0, padx=10, pady=5, sticky=tk.W)
+        self.symptoms_entry.grid(row=7, column=1, pady=10, padx=5)
 
-    self.add_button.grid(row=8, column=0, padx=10, pady=5)
-    self.search_button.grid(row=8, column=1, padx=10, pady=5)
+        # Place buttons on grid
+        self.add_button.grid(row=8, column=0, padx=10, pady=5)
+        self.search_button.grid(row=8, column=1, padx=10, pady=5)
 
-    # provide instance
-    self.contact_tracing = C19CTAppGUI
+        # provide instance
+        self.contact_tracing = CCTracingApp()
 
-    # add entry
+    # add_entry
     def add_entry(self):
         name = self.name_entry.get()
         address = self.address_entry.get()
@@ -84,12 +83,13 @@ class C19CTAppGUI:
         exposed = self.exposed_entry.get()
         symptoms = self.symptoms_entry.get()
 
+
         self.contact_tracing.add_entry(name, address, birthday, gender, phone, email, exposed, symptoms)
 
         self.clear_entries()
         print("The information entry is submitted successfully.")
 
-    # search entry
+    # search_entry
     def search_entry(self):
         name = self.name_entry.get()
 
@@ -97,11 +97,11 @@ class C19CTAppGUI:
         entry = self.contact_tracing.search_entry(name)
 
         if entry:
-            print("Submitted Information Found:")
+            print("Entry found:")
             for item in entry:
                 print(item)
         else:
-            print("Submitted Information not found")
+            print("Entry not found")
 
     # check errors
     def clear_entries(self):
@@ -113,10 +113,10 @@ class C19CTAppGUI:
         self.exposed_entry.delete(0, tk.END)
         self.symptoms_entry.delete(0, tk.END)
 
-    # run the program
-    if __name__ == "__main__":
-        root = tk.Tk()
+# run the program
+if __name__ == "__main__":
+    root = tk.Tk()
 
-        app = C19CTAppGUI(root)
+    app = CovidContactTracingAppGUI(root)
 
-        root.mainloop()
+    root.mainloop()
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 print("Final Project".center(38, "="))
 print("\033[93m=" * 38)
-print("\033[95mCOVID-19 CONTACT TRACING APP WITH GUI")
+print("\033[95mCOVID-19 CONTACT TRACING AP WITH GUI")
 print("\033[93m=" * 38)
 
 # pseudocode
 # import a text file
+'''A comma-separated values file is a text file that uses a comma to separate values, giving rise to the name of this file format.'''
 import csv
 
 # create the class that will represent the app
-class Covid19ContactTracingApp:
+class CCTracingApp:
     def __init__(self):
         pass
+
     # establish the functions
     '''Record the gathered data in a CSV file.'''
-
-    def add_entry(self, name, birthday, gender, number, email, address, exposed, symptoms):
-        with open("covid-19_contact_tracing_app.csv", "a", newline="") as file:
+    def add_entry(self, name, bday, gender, phone, email, address, date, result):
+        with open("contact_tracing.csv", "a", newline="") as file:
             writer = csv.writer(file)
-            writer.writerow([name, address, birthday, gender, number, email, exposed, symptoms])
+            writer.writerow([name, bday, gender, phone, email, address, date, result])
 
     '''Extract data from the CSV file and look for a corresponding name.'''
     def search_entry(self, name):
         entries = []
-        with open("covid-19_contact_tracing_app.csv", "r") as file:
+        with open("contact_tracing.csv", "r") as file:
             reader = csv.reader(file)
             for row in reader:
 
